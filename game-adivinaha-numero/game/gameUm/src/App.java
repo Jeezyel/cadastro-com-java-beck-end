677c32cef25d3d22604a53f2a7363cd4f250a766
Jeezyel <jeezyelpereira@gmail.com>
2022-07-23 20:57:26 -0300

    cirando metodos mais importantes

--- game-adivinaha-numero/game/gameUm/src/App.java
+++ game-adivinaha-numero/game/gameUm/src/App.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class App {
+    public static Scanner leia = new Scanner(System.in);
+    public static List<Integer> chutes = new ArrayList<Integer>();
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+    public static void menu(){
+
+    }
+    public static void armazenarChutes(Integer Integer){
+
+    }
+    public static void escolaNumero(){
+
+
+        
     }
 }
